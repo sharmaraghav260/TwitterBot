1f59d4a2ede86f3278e8521ac07f19789ff501aa
sharmaraghav260 <sharmaraghav260@gmail.com>
2017-06-05 22:13:09 -0600

    splitted bots

---
+++ randomNumberTweetBot.js
@@ -0,0 +1,30 @@
+console.log("The follow bot is starting");
+
+var Twit = require('twit');
+var config = require('./config');
+var T = new Twit(config);
+
+
+
+tweetIt();
+setInterval(tweetIt, 1000*10);
+
+function tweetIt(){
+  var r = Math.floor(Math.random()*100);
+
+
+  var tweet = {
+    status: 'here is a random number ' + r + ' #coding from node.js'
+  };
+  //  tweet 'hello world!'
+  T.post('statuses/update', tweet, tweeted);
+
+  function tweeted (err, data, response) {
+    if(err){
+      console.log("Something went wrong!");
+      console.log(data);
+    }else{
+      console.log("It worked!");
+    }
+  }
+}
