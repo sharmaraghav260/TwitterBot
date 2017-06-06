4b529c6abedaae39e090b2b006004d978eea8e65
sharmaraghav260 <sharmaraghav260@gmail.com>
2017-06-06 17:15:47 -0600

    more organized

---
+++ node2_searchbot/config.js
@@ -0,0 +1,7 @@
+module.exports ={
+consumer_key:         'uBDm0mtLoS5RSF7bolPASVKKY',
+consumer_secret:      'W4Bic6gNwnmwyF4PPCS6mLOTZiypFeWozCdl9O4RFqzRpVKFSj',
+access_token:         '871218946371866628-3gJaZ1JMXXVgCXHLoZOvbnl3UTustat',
+access_token_secret:  'gRUylUFEWVDyplDNsMkvqTgJpkMwgFhMzHNmeeYNrSoy5',
+timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
+}
