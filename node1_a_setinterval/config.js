a11930e720936553ee4ea01249cb0e557ab8c3fa
sharmaraghav260 <sharmaraghav260@gmail.com>
2018-02-06 14:04:53 -0700

    Api keys removed

--- node1_a_setinterval/config.js
+++ node1_a_setinterval/config.js
@@ -1,7 +1,8 @@
 module.exports ={
-consumer_key:         'uBDm0mtLoS5RSF7bolPASVKKY',
-consumer_secret:      'W4Bic6gNwnmwyF4PPCS6mLOTZiypFeWozCdl9O4RFqzRpVKFSj',
-access_token:         '871218946371866628-3gJaZ1JMXXVgCXHLoZOvbnl3UTustat',
-access_token_secret:  'gRUylUFEWVDyplDNsMkvqTgJpkMwgFhMzHNmeeYNrSoy5',
-timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
+    consumer_key:         'Your consumer key',
+    consumer_secret:      'Your consumer secret',
+    access_token:         'Your access token',
+    access_token_secret:  'Your acess token secret',
+    timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
 }
+    
